a9408de882c0d512d2e365b594ebc5699e5c6e96
Steve Anderson <prototype909@msn.com>
2020-05-07 22:04:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    puts = "I LOVE YOU TOO PUMPKIN!"
+    respond = "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
-    puts = "NO, NOT SINCE 1938!"
+    respond = "NO, NOT SINCE 1938!"
   else
-    puts = "HUH?! SPEAK UP, SONNY!"
+    respond = "HUH?! SPEAK UP, SONNY!"
   end
 end
